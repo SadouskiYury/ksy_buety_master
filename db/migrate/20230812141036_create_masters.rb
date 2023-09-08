2f85fc2d5c59507095057fdfbaf3f0e970a5bcd2
Yury Sadouski <yury.sadouski@softswiss.com>
2023-09-08 08:58:47 +0300

    update annatotons and migration

--- db/migrate/20230812141036_create_masters.rb
+++ db/migrate/20230812141036_create_masters.rb
@@ -3,13 +3,12 @@
 class CreateMasters < ActiveRecord::Migration[7.0]
   def change
     create_table :masters, id: :uuid do |t|
-      t.text :name, null: false
+      t.text :name, null: false, index: { unique: true }
       t.text :about_me
 
       t.timestamps
     end
 
-    add_index :masters, :name, unique: true
     add_reference :certificates, :master, foreign_key: true, type: :uuid
     add_reference :articles, :master, foreign_key: true, type: :uuid
     add_reference :contacts, :master, foreign_key: true, type: :uuid
--- spec/factories/admin_masters.rb
+++ spec/factories/admin_masters.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-# == Schema Information
-#
-# Table name: admin_masters
-#
-#  id         :uuid             not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  admin_id   :uuid
-#  master_id  :uuid
-#
 FactoryBot.define do
   factory :admin_master do
     admin
