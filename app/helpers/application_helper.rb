072f0ccd987fd44fb97ac195b6017cea6ef46017
Yury Sadouski <yury.sadouski@softswiss.com>
2023-09-11 12:04:55 +0300

    implemnted home page with needed libraries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def page_title
+    "Beauty Master Kseniya | #{controller_name.humanize}"
+  end
+
+  def body_class
+    "#{controller_name}-#{action_name}"
+  end
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,19 +1,20 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+// const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
+    './app/assets/stylesheets/**/*.css',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
+  // theme: {
+  //   extend: {
+  //     fontFamily: {
+  //       sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+  //     },
+  //   },
+  // },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
